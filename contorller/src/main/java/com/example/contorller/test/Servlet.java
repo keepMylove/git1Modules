59273627ed1222c42d3d3ff1034b1b08fe7f53c9
Administrator <2815418919@qq.com>
2022-08-29 17:30:32 +0800

    first

--- contorller/src/main/java/com/example/contorller/test/Servlet.java
+++ contorller/src/main/java/com/example/contorller/test/Servlet.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class Servlet {
-    @RequestMapping("/")
+    @RequestMapping("/a")
     @ResponseBody
     public String user(){
         User user = new User();
